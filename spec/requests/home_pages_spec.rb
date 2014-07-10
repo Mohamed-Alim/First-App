83b51ddbabff4c941e7f6882acd3b93368e14b48
Mohamed H. Abdelalim <mohamedhussam29@gmail.com>
2014-07-10 12:22:16 +0200

    changes

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Home Pages", :type => :request do
- describe " Home Page" do
- 	it "should have the content'First app'" do
+ describe "Home Page" do
+ 	it "should have the content'Home'" do
  		visit 'home_page/home'
- 		page.should have_content('First app')
+ 		page.should have_content('Home')
+ 	end
+ 	describe "About page" do
+ 	it "should have the content'About'" do
+ 		visit 'home_page/home'
+ 		page.should have_content('About')
  	end
   end
 end
