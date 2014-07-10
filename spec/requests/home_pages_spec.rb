d90293ed5f3068191b43b4d805f7a95f391a0917
Mohamed H. Abdelalim <mohamedhussam29@gmail.com>
2014-07-10 13:27:32 +0200

    update

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -2,14 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "Home Pages", :type => :request do
  describe "Home Page" do
- 	it "should have the content'Home'" do
+ 	it "should have the content'Our Demo App'" do
  		visit 'home_page/home'
- 		page.should have_content('Home')
+ 		page.should have_content('Our Demo App')
+ 		page.should have_selector('h1', :text => 'Our Demo App')
+ 		expect(page).to have_title('Demo App | Home')
  	end
+ end
  	describe "About page" do
- 	it "should have the content'About'" do
- 		visit 'home_page/home'
- 		page.should have_content('About')
+	 	it "should have the content'About us'" do
+	 		visit 'home_page/about'
+	 		page.should have_content('About us')
+	 		page.should have_selector('h1', :text => 'About us')
+	 		expect(page).to have_title('Demo App | About us')
  	end
+	 	end
+	 end
+	  describe "Location Page" do
+	 	it "should have the content'Location'" do
+	 		visit 'home_page/location'
+	 		page.should have_content('Location')
+	 		page.should have_selector('h1', :text => 'Location')
+	 		expect(page).to have_title('Demo App | Location')
+	 	end
   end
+  describe "Contact Page" do
+	 	it "should have the content'Contact us'" do
+	 		visit 'home_page/contact'
+	 		page.should have_content('Contact us')
+	 		page.should have_selector('h1', :text => 'Contact us')
+	 		expect(page).to have_title('Demo App | Contact us')
+
+	 	end
+	 end
+
+	 #excpect(page).to have_title('About Us')
+	
 end
