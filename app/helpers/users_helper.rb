bd1b239f2cf637b8e7dd48da7f5e7c99e2743b84
Mohamed H. Abdelalim <mohamedhussam29@gmail.com>
2014-07-27 23:32:46 +0200

    update

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,8 @@ module UsersHelper
 	#returns the Gravatar for the given user
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 		image_tag(gravatar_url, alt: user.name, class:"gravatar")
 	end
 end
